54bbe5f5a621c280035d41fc172968bedb41bced
Frank <franksu49@gmail.com>
2020-01-06 22:55:36 -0500

    Updates

---
+++ precache-manifest.5c05954cc29c1b5d1d6f0aab0e456bac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad6ade27c34158684610443024202a8",
+    "url": "/VisuallyGraphic/index.html"
+  },
+  {
+    "revision": "dbab61e4c99b17ebec72",
+    "url": "/VisuallyGraphic/static/css/main.fecda765.chunk.css"
+  },
+  {
+    "revision": "8dd1e140fd74601595e2",
+    "url": "/VisuallyGraphic/static/js/2.7c5aa08c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/VisuallyGraphic/static/js/2.7c5aa08c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbab61e4c99b17ebec72",
+    "url": "/VisuallyGraphic/static/js/main.9b427ccb.chunk.js"
+  },
+  {
+    "revision": "5fa295e7db4f22baf872",
+    "url": "/VisuallyGraphic/static/js/runtime-main.be73a1e5.js"
+  },
+  {
+    "revision": "539ab07e33a0427e45211c529d8f6196",
+    "url": "/VisuallyGraphic/static/media/circle.539ab07e.svg"
+  },
+  {
+    "revision": "49fad9e6bfd5a3db8e62ba07fe0496d9",
+    "url": "/VisuallyGraphic/static/media/triangletwo-right.49fad9e6.svg"
+  }
+]);
